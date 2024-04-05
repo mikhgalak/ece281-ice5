9ab859c132465ce03372f8986ab6ed6e1a63a718
mikhgalak <mikhailvgalaktionov@gmail.com>
2024-04-04 21:42:10 -0600

    Gradescope Test

--- src/hdl/elevator_controller_fsm_tb.vhd
+++ src/hdl/elevator_controller_fsm_tb.vhd
@@ -103,6 +103,7 @@ begin
         w_reset <= '1';  wait for k_clk_period;
             assert w_floor = "0010" report "bad reset" severity failure; 
         -- clear reset
+        w_reset <= '0'; wait for k_clk_period;
 		
 		-- active UP signal
 		w_up_down <= '1'; 
@@ -112,8 +113,46 @@ begin
             assert w_floor = "0010" report "bad wait on floor2" severity failure;
         w_stop <= '0';  wait for k_clk_period;
             assert w_floor = "0011" report "bad up from floor2" severity failure;
-		-- rest of cases
-        
+	
+	
+         -- stay on each o_floor for 2 cycles and then move up to the next o_floor
+        w_stop <= '1';  wait for k_clk_period * 2;
+           assert w_floor = "0011" report "bad wait on floor3" severity failure;
+        w_stop <= '0'; wait for k_clk_period;
+           assert w_floor = "0100" report "bad up from floor3" severity failure;
+       
+           -- go back DOWN
+        w_up_down <= '0'; 
+       
+           -- stay on each o_floor for 2 cycles and then move down to the next o_floor
+        w_stop <= '1';  wait for k_clk_period * 2;
+           assert w_floor = "0100" report "bad wait on floor4" severity failure;
+        w_stop <= '0';  wait for k_clk_period;
+           assert w_floor = "0011" report "bad down from floor4" severity failure;
+       
+           -- stay on each o_floor for 2 cycles and then move down to the next o_floor
+        w_stop <= '1';  wait for k_clk_period * 2;
+           assert w_floor = "0011" report "bad wait on floor3" severity failure;
+        w_stop <= '0';  wait for k_clk_period;
+           assert w_floor = "0010" report "bad down from floor3" severity failure;
+       
+        w_stop <= '1';  wait for k_clk_period * 2;
+           assert w_floor = "0010" report "bad stop on floor4" severity failure;
+        w_stop <= '0';  wait for k_clk_period;
+           assert w_floor = "0010" report "bad stop on floor4" severity failure;
+       
+           -- go back DOWN
+        w_up_down <= '0'; 
+       
+           -- stop at bottom floor
+        w_stop <= '1';  wait for k_clk_period * 2;
+           assert w_floor = "0010" report "bad stop on bottom floor" severity failure;
+        w_stop <= '0';  wait for k_clk_period;
+           assert w_floor = "0010" report "bad stop on bottom floor" severity failure;
+       
+           -- stay on bottom floor for 2 cycles
+        w_stop <= '1';  wait for k_clk_period * 2;
+           assert w_floor = "0010" report "bad wait on bottom floor" severity failure;
         -- go back DOWN
           
 		  	
